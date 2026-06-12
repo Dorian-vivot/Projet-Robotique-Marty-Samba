cb46c36eeb7fe9260999ec5f5f2d59a7e921e188
Dorian-vivot <dodo@PC-Portable-Dorian.>
2026-06-12 10:48:27 +0200

    Ajout de la gestion des erreurs pour les fonctions de déplacements

--- Joueur/martyConnection.py
+++ Joueur/martyConnection.py
@@ -134,43 +134,61 @@ class MartyConnection(QObject):
         
 
     def turn(self, direction: str):
-        if self._marty is not None and self._is_connected:
-            turn_val = 0
-            if direction == "right":
-                turn_val = 180
-            elif direction == "left":
-                turn_val = -180
-            self._marty.walk(num_steps=1, start_foot="auto", turn=turn_val, step_length=0, move_time=5000)
+        try:
+            if self._marty is not None and self._is_connected:
+                turn_val = 0
+                if direction == "right":
+                    turn_val = 180
+                elif direction == "left":
+                    turn_val = -180
+                self._marty.walk(num_steps=1, start_foot="auto", turn=turn_val, step_length=0, move_time=5000)
+        except Exception as e:
+            print(f"Erreur lors de l'éxécution du demi-tour (Direction : {direction}) : {e}")
 
     def stop(self):
-        if self._marty is not None and self._is_connected:
-            self._marty.stop()
+        try:
+            if self._marty is not None and self._is_connected:
+                self._marty.stop()
+        except Exception as e:
+            print(f"Erreur pour stopper l'éxécution : {e}")
 
     def moveArm(self, side: str, movement: str):
-        if self._marty is not None and self._is_connected:
-            joint = ""
-            if side == "left":
-                joint = "left arm"
-            elif side == "right":
-                joint = "right arm"
+        try:
+            if self._marty is not None and self._is_connected:
+                joint = ""
+                if side == "left":
+                    joint = "left arm"
+                elif side == "right":
+                    joint = "right arm"
 
-            angle = self._marty.get_joint_position(joint_name_or_num=joint)
-            if movement == "raise":
-                angle += 45
-            elif movement == "back":
-                angle -= 45
-            self._marty.move_joint(joint_name_or_num=joint, position=angle, move_time=500)
+                angle = self._marty.get_joint_position(joint_name_or_num=joint)
+                if movement == "raise":
+                    angle += 45
+                elif movement == "back":
+                    angle -= 45
+                self._marty.move_joint(joint_name_or_num=joint, position=angle, move_time=500)
+        except Exception as e:
+            print(f"Erreur pour bouger le bras (Direction : {side}, Mouvement : {movement}) : {e}")
 
     def armsNeutral(self):
-        if self._marty is not None and  self._is_connected:
-            self._marty.move_joint(joint_name_or_num="left arm",  position=0, move_time=500)
-            self._marty.move_joint(joint_name_or_num="right arm", position=0, move_time=500)
-
+        try:
+            if self._marty is not None and  self._is_connected:
+                self._marty.move_joint(joint_name_or_num="left arm",  position=0, move_time=500)
+                self._marty.move_joint(joint_name_or_num="right arm", position=0, move_time=500)
+        except Exception as e:
+            print(f"Erreur pour bouger les bras à la position initial : {e}")
+        
     def setEyesPose(self, pose: str):
-        if self._marty is not None and self._is_connected:
-            self._marty.eyes(pose_or_angle=pose, move_time=300)
+        try:
+            if self._marty is not None and self._is_connected:
+                self._marty.eyes(pose_or_angle=pose, move_time=300)
+        except Exception as e:
+            print(f"Erreur lors de l'éxécution de la commmande eyes (Pose : {pose}) : {e}")
 
     def setExpression(self, pose: str, color: tuple):
-        if self._marty is not None and self._is_connected:
-            self._marty.eyes(pose_or_angle=pose, move_time=300)
-            self._marty.disco_color(color, api='led')
+        try:
+            if self._marty is not None and self._is_connected:
+                self._marty.eyes(pose_or_angle=pose, move_time=300)
+                self._marty.disco_color(color, api='led')
+        except Exception as e:
+            print(f"Erreur lors de l'éxécution de l'expression (Pose : {pose}, Couleur : {color}) : {e}")
